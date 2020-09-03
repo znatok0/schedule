0da892dcd40aa5973a3a181ebba52a1eb9c3df47
znatok0 <galiaskar.uli@gmail.com>
2020-09-03 08:04:41 -0400

    design fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,19 +20,21 @@
     <!-- Header -->
     <header>
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="/img/index.png" alt="logo"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nac-item">
-                        <form class="form-inline">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Поиск по группе" aria-label="Search">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
-                        </form>
-                    </li>
-                </ul>
+            <div class="header-content">
+                <a class="navbar-brand" href="{{ url('/') }}"><img class="logo" src="/img/index.png" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nac-item">
+                            <form class="form-inline">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Поиск группы, преподавателя, аудитории" aria-label="Search">
+                                <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button> -->
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
