bb782d8db0728a03c3d5df89d246811e644a4efa
znatok0 <galiaskar.uli@gmail.com>
2020-09-04 06:52:30 -0400

    some fixxes

--- index.php
+++ index.php
@@ -21,8 +21,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-// require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/bootstrap/autoload.php';
+require __DIR__.'/krmu-dot-2.kz/vendor/autoload.php';
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +35,7 @@ require __DIR__.'/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/krmu-dot-2.kz/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
